dd179abdeeaeb9c474d91659920bdb6ed6482e56
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2023-12-06 00:45:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function sim() {
+    location.href="https://www.youtube.com/watch?v=izGwDsrQ1eQ"
+}
+
+function desvia(t) {
+    var btn = t;
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left =  geraPosicao(10, 90);
+    console.log("opa, desviei...")            
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
